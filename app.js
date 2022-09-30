9fb43664e26ae74240bccb06b991a3388cb8c316
dev-shakib <95518941+dev-shakib@users.noreply.github.com>
2022-09-30 17:37:49 +0600

    1 file modified, 1 added

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var app = express();
-
 app.set('view engine', 'ejs');
 
 var names = ['john', 'joe', 'Shakib', 'rakib'];
-
 var text = 'This is my text';
 
 app.get('/index', (req, res) => {
   res.render('page1', { names: names });
 });
-
 app.get('/about', (req, res) => {
   res.render('page2');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('form');
 });
 
 app.listen(3000, () => {
